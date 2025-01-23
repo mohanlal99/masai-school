7069b2d2bdcc8e3d7991756b772264e2ecb118c3
mohanlal99 <mohanlalv433@gmail.com>
2025-01-23 16:21:49 +0530

    Day 1

---
+++ index.js
@@ -0,0 +1,73 @@
+// // // 
+// // let days = [30,28,29,27,25,40,45]
+// // for(let i = 0;i<days.length;i++){
+// // i==3&&(days[i] = days[i] + 1)
+// // }
+// // console.log(days)
+
+// alert("Helo");
+
+
+// // let name = prompt("Hello enter Your Name");
+// function greetUser(name="Guest"){
+// console.log(`Hello, ${name}!`)
+// }
+
+// greetUser()
+// console.log("Frist task start")
+// let work = [1,2,3,4,5]
+// let newWork = []
+// for(let i = 1;i<work.length;i++){
+// newWork.push(work[i])
+// }
+// console.log(newWork)
+
+
+// console.log("task second start")
+
+// let addNewArray =  []
+// let count = 0;
+// for(let i = 0 ;i<=newWork.length;i++){
+//   let nw = i+"newWork"
+//   if(i<2&&count!=2){
+//     addNewArray.push(nw)
+//     count++
+//     if(count==2){
+//       i=0
+//     }
+//   }else{
+//     addNewArray.push(newWork[i-1])
+//   }
+// }
+// console.log(addNewArray)
+
+// console.log("Last task replace with new Task")
+// addNewArray[addNewArray.length -1] = "LAST Task replace"
+// console.log(addNewArray)
+// console.log("Thanks for reading")
+
+
+function calculatePrice(discount = 10,product_price){
+    product_price
+    return discount
+}
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
