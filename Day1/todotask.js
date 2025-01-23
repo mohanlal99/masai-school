393a0cc7d16ddd53f5e693e90377354aa412b068
mohanlal99 <mohanlalv433@gmail.com>
2025-01-23 21:30:22 +0530

    Todotask update

---
+++ Day1/todotask.js
@@ -0,0 +1,7 @@
+
+console.log("Welcome To todo list app")
+
+let task = ["Task1","Task2","Task3","Task4","Task5"]
+console.log(task.unshift("NewTask1")) // add bigning in task this is unshift method and add in bigning
+console.log(task.pop()) // remove last using pop method this is array method remoev last method and return remove method 
+console.log(task) //finally log the updated todo list app 
