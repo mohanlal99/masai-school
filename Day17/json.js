355a8e721e704bfb0831f29b3cd1ea084863710e
mohanlal99 <mohanlalv433@gmail.com>
2025-02-19 22:59:22 +0530

    Add new assignment problem

---
+++ Day17/json.js
@@ -0,0 +1,19 @@
+let userData = {
+    name:'mohanlal',
+    age:21,
+    course:'Web Development'
+  }
+let userData2 = {
+    name:'mohanlal',
+    age:43,
+    course:'Software Enginer'
+  }
+let userData3 = {
+    name:'pawan',
+    age:24,
+    course:'Software Development'
+  }
+  
+  console.log(JSON.stringify(userData))
+  console.log(JSON.stringify(userData2))
+  console.log(JSON.stringify(userData3))
