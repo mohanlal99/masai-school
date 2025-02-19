5b44a7535afcba71ffbd0d581a508d521bd20627
mohanlal99 <mohanlalv433@gmail.com>
2025-02-19 18:55:45 +0530

    Add new assignment problem

---
+++ Day17/evenwitharr.js
@@ -0,0 +1,18 @@
+function filterEvenNumbers(arr){
+    return arr.filter(item=>item%2==0)
+  }
+  function sumOfArray(arr){
+    return arr.reduce((total,item)=>total+item,0)
+  }
+  function sortAndConcat(arr1, arr2){
+    // let data = arr.concat(arr2)
+    arr1.sort((a,b)=>a-b)
+    arr2.sort((a,b)=>a-b)
+    
+    return [...arr1, ...arr2]
+  }
+  
+  let arr = [1,5,357,2,3,49,7,09,2,8,3454,0]
+  console.log(filterEvenNumbers(arr))
+  console.log(sumOfArray(arr))
+  console.log(sortAndConcat([23,7,5,08,23,9,27],[29,4,58,65,92,3,74,589]))
